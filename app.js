cb0b2a7ddffb8a587168e86945096af334cb846f
Lance Shaw <AmbroseSouth@outlook.com>
2022-01-27 06:25:55 -0600

    completed exercise

---
+++ app.js
@@ -0,0 +1,31 @@
+// function makePerson(first, last, age) {
+//     return {
+//         first: first,
+//         last: last,
+//         age: age,
+//         isAlive: true
+//     }
+// }
+
+function makePerson(first, last, age) {
+    return {
+        first,
+        last,
+        age,
+        isAlive: true,
+    }
+}
+
+const mathStuff = {
+    x: 200,
+    add(a, b) {
+        return a + b;
+    },
+    square(a) {
+        return a ** 2;
+    },
+}
+
+{
+    periwinkle: ""
+}
